d62a124d19427562779d2f57909ffaa902985293
novikovsc <81076610+novikovsc@users.noreply.github.com>
2022-03-30 09:48:57 +0300

    create new projects

---
+++ Collection/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Collection
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            List<Entity> list = new List<Entity>()
+            {
+                new Entity { Id = 1, ParentId = 0, Name = "Root entity" },
+                new Entity { Id = 2, ParentId = 1, Name = "Child of 1 entity" },
+                new Entity { Id = 3, ParentId = 1, Name = "Child of 1 entity" },
+                new Entity { Id = 4, ParentId = 2, Name = "Child of 2 entity" },
+                new Entity { Id = 5, ParentId = 4, Name = "Child of 4 entity" }
+            };
+
+
+            foreach (var entity in returnEntity(list))
+            {
+                Console.WriteLine(entity.Key + " " + entity.Value);
+            }
+        }
+
+        public static Dictionary<int, List<Entity>> returnEntity(List<Entity> list)
+        {
+            Dictionary<int, List<Entity>> entity = new Dictionary<int, List<Entity>>();
+
+            foreach (var ent in list)
+            {
+                entity[ent.ParentId] = new List<Entity>()
+                {
+                    ent
+                };
+            }
+
+            return entity;
+        }
+    }
+
+    class Entity
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Logger/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Logger
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+         {
+             LocalFileLogger<int> logger = new LocalFileLogger<int>(@"D:\Docs\logger.txt");
+             
+             logger.LogInfo("LogInfo");
+             logger.LogWarning("LogWarning");
+             logger.LogError("LogWarning");
+         }
+    }
+
+    class LocalFileLogger<T>
+    {
+        private string path;
+
+        public LocalFileLogger(string message)
+        {
+            path = message;
+        }
+
+        public void LogInfo(string message)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine($"[Info]: [{typeof(T).Name}] : {message}");
+            }
+        }
+        
+        public void LogWarning(string message)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine($"[Warning]: [{typeof(T).Name}] : {message}");
+            }
+        }
+        
+        public void LogError (string message)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine($"[Error]: [{typeof(T).Name}] : {message}");
+            }
+        }
+    }
+
+}
+
